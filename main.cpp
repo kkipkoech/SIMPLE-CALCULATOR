6ad1baeacff619d07077994f3c4acabc6ba0df61
kkipkoech <124553562+kkipkoech@users.noreply.github.com>
2023-02-28 18:50:30 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+  int first_number, second_number, sum;
+
+  cout << "Enter two integers: ";
+  cin >> first_number >> second_number;
+
+  // sum of two numbers in stored in variable sumOfTwoNumbers
+  sum = first_number + second_number;
+
+  // prints sum
+  cout << first_number << " + " <<  second_number << " = " << sum;
+
+  return 0;
+  {
+  int first number,second number,subtraction
+  cout<<"enter two integers";
+  cin>>first number>>second number;
+  //the difference between the two numbers is stored in the vaiable suboftwonumbers
+  sub= first_number-second_number;
+  //prints difference
+  cout<<first_number<<"-"<<second_number<<"="<<sub;
+  return 0}
+}
